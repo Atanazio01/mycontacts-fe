d425fb69b912f9f6397887edd062d0b5dbf3a734
Atanazio01 <marcosd.atanazio@gmail.com>
2022-03-22 16:47:39 -0300

    Moving the request into the Home Component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import {
@@ -9,6 +10,21 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/contacts')
+      .then(async (response) => {
+        const json = await response.json();
+        setContacts(json);
+      })
+      .catch((error) => {
+        console.log('erro: ', error);
+      });
+  }, []);
+
+  console.log(contacts);
+
   return (
     <Container>
       <InputSearchContainer>
@@ -51,18 +67,6 @@ export default function Home() {
     </Container>
   );
 }
-
-fetch('http://localhost:3001/contacts')
-  .then(async (response) => {
-    const json = await response.json();
-    console.log('resposta', response);
-    json.forEach((contact) => {
-      console.log(contact.name);
-    });
-  })
-  .catch((error) => {
-    console.log('erro: ', error);
-  });
 
 // SOP -> Same Origin Policy -> Política de mesma origem
 // CORS -> Cross-Origin Resource Sharing -> Compartilhamento de recursos entre origens cruzadas.
