ac25b2a3b9e99268b6e3fda4cd68beb3fd1772c3
Atanazio01 <marcosd.atanazio@gmail.com>
2025-01-23 13:21:26 -0300

    Changing the CustomEvent to our EventManager

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -2,26 +2,27 @@ import { useState, useEffect } from 'react';
 import { Container } from './styles';
 import ToastMessage from '../ToastMessage';
 
+import { toastEventManager } from '../../../utils/toast';
+
 export default function ToastContainer() {
-  const [messages, setMessages] = useState([
+  const [messages, setMessages] = useState([/*
     { id: Math.random(), type: 'default', text: 'Default text' },
     { id: Math.random(), type: 'danger', text: 'Danger text' },
     { id: Math.random(), type: 'success', text: 'Success text' },
-  ]);
+   */]);
 
   useEffect(() => {
-    function handleAddToast(event) {
-      const { type, text } = event.detail;
+    function handleAddToast({ type, text }) {
       setMessages((prevState) => [
         ...prevState,
         { id: Math.random(), type, text },
       ]);
     }
 
-    document.addEventListener('addtoast', handleAddToast);
+    toastEventManager.on('addtoast', handleAddToast);
 
     return () => {
-      document.removeEventListener('addtoast', handleAddToast);
+      toastEventManager.removeListener('addtoast', handleAddToast);
     };
   }, []);
 
