7b7656c12a57a522566f63ece8a92b49213ff762
Atanazio01 <marcosd.atanazio@gmail.com>
2025-09-23 15:29:49 -0300

    feat: implementing the toDomain method in ContactMapper

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -6,23 +6,27 @@ class ContactsService {
     this.HttpClient = new HttpClient('http://localhost:3001');
   }
 
-  listContacts(orderBy = 'asc') {
-    return this.HttpClient.get(`/contacts?orderBy=${orderBy}`, {
-    });
+  async listContacts(orderBy = 'asc') {
+    const contacts = await this.HttpClient.get(`/contacts?orderBy=${orderBy}`);
+
+    return contacts.map(ContactMapper.toDomain);
   }
 
-  getContactById(id) {
-    return this.HttpClient.get(`/contacts/${id}`, {
-    });
+  async getContactById(id) {
+    const contact = await this.HttpClient.get(`/contacts/${id}`);
+
+    return ContactMapper.toDomain(contact);
   }
 
   createContact(contact) {
     const contactData = ContactMapper.toPersistence(contact);
+
     return this.HttpClient.post('/contacts', { body: contactData });
   }
 
   updateContact(id, contact) {
     const contactData = ContactMapper.toPersistence(contact);
+
     return this.HttpClient.put(`/contacts/${id}`, { body: contactData });
   }
 
