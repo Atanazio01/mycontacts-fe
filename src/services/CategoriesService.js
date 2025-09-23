882836d5a15132c4913db527508deadea370366f
Atanazio01 <marcosd.atanazio@gmail.com>
2025-09-23 15:43:56 -0300

    feat: creating CategoryMapper

--- src/services/CategoriesService.js
+++ src/services/CategoriesService.js
@@ -1,3 +1,4 @@
+import CategoryMapper from './mappers/CategoryMapper';
 import HttpClient from './utils/HttpClient';
 
 class CategoriesService {
@@ -5,8 +6,32 @@ class CategoriesService {
     this.HttpClient = new HttpClient('http://localhost:3001');
   }
 
-  listCategories() {
-    return this.HttpClient.get('/categories');
+  async listCategories() {
+    const categories = await this.HttpClient.get('/categories');
+
+    return categories.map(CategoryMapper.toDomain);
+  }
+
+  async getCategoryById(id) {
+    const category = await this.HttpClient.get(`/categories/${id}`);
+
+    return CategoryMapper.toDomain(category);
+  }
+
+  async createCategory(name) {
+    const category = await this.HttpClient.post('/categories', { body: { name } });
+
+    return CategoryMapper.toDomain(category);
+  }
+
+  async updateCategory(id, name) {
+    const category = await this.HttpClient.put(`/categories/${id}`, { body: { name } });
+
+    return CategoryMapper.toDomain(category);
+  }
+
+  deleteCategory(id) {
+    return this.HttpClient.delete(`/categories/${id}`);
   }
 }
 
