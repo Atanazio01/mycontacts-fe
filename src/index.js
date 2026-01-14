3d99b5917167fb9dca631a87a7170d7eed572155
Atanazio01 <marcosd.atanazio@gmail.com>
2026-01-14 11:07:42 -0300

    feature: enable strict mode

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { ThemeProvider } from 'styled-components';
 import App from './components/App';
+import GlobalStyles from './assets/styles/global';
+import defaultTheme from './assets/styles/themes/default';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <App />,
+  <ThemeProvider theme={defaultTheme}>
+    <GlobalStyles />
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ThemeProvider>,
 );
